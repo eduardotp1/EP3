9694e13812eb27067124c817f0e2d050d4068f1e
eduardotp1 <edu.tirta@gmail.com>
2016-04-24 01:08:35 -0300

    classe jogo com pequenos problemas

--- jogo.py
+++ jogo.py
@@ -7,22 +7,23 @@ Created on Tue Apr 19 16:43:25 2016
 import numpy as np
 
 class jogo:
-    
+    matriz=np.array([[0,1,2],[10,11,12],[20,21,22]])
+    rodadas=0
     def __init__(self,matriz,rodadas):
-        self.matriz=np.array([[0,1,2],[10,11,12],[20,21,22]])
-        self.rodadas=0
+        self.matriz=matriz
+        self.rodadas=rodadas
         
     def recebe_jogadas(self,linha,coluna):
-        if self.rodadas==0 or 2 or 4 or 6 or 8:
-            self.matriz[(linha)][(coluna)]="X"
+        if self.rodadas%2==0:
+            self.matriz[linha][coluna]=="X"
             self.rodadas+=1
             
         else:
-            self.matriz[(linha)][(coluna)]="O"
+            self.matriz[linha][coluna]=="O"
             self.rodadas+=1
                         
-    def verifica_ganhador(self):
-        if self.rodadas>=4:
+    def verifica_ganhador(self,rodadas):
+        if self.rodadas>=4:#primeira oportunidade de acabar o jogo
             #quando X eh vencedor
             if self.matriz[0][0]=="X" and self.matriz[0][1]=="X" and self.matriz[0][2]=="X":
                 return 1
@@ -57,12 +58,14 @@ class jogo:
                 return 2
             elif self.matriz[0][2]=="O" and self.matriz[1][1]=="O" and self.matriz[2][0]=="O":
                 return 2   
+            #para as jogadas emmpatadas deve estar na 9 rodada e nao pode ter 
             elif self.rodadas==8:
                 return 0
     def limpa_jogadas(self):
         if self.verifica_ganhador()==0:
             print ('empatou')
             self.matriz=np.array([[0,1,2],[10,11,12],[20,21,22]])
+            self.rodadas+=1
         elif self.verifica_ganhador()==1:
             print('jogador X ganhou')
             self.matriz=np.array([[0,1,2],[10,11,12],[20,21,22]])
@@ -70,9 +73,18 @@ class jogo:
             print('jogador O ganhou')
             self.matriz=np.array([[0,1,2],[10,11,12],[20,21,22]])
         return self.matriz
-for i in range (0,3):
-    for j in range (0,3):
-        jogo.recebe_jogadas(i,j)
-        jogo.verifica_ganhador()
-print (jogo.matriz)
 
+print (jogo.matriz)
+#jogo.verifica_ganhador(jogo,jogo.rodadas)
+jogo.recebe_jogadas(jogo,0,0)
+#jogo.verifica_ganhador(jogo,jogo.rodadas)
+jogo.recebe_jogadas(jogo,1,0)
+#jogo.verifica_ganhador(jogo,jogo.rodadas)
+jogo.recebe_jogadas(jogo,0,1)
+#jogo.verifica_ganhador(jogo,jogo.rodadas)
+jogo.recebe_jogadas(jogo,1,2)
+#jogo.verifica_ganhador(jogo,jogo.rodadas)
+jogo.recebe_jogadas(jogo,0,2)
+#jogo.verifica_ganhador(jogo,jogo.rodadas)
+print(jogo.matriz)
+#print (jogo.verifica_ganhador(jogo,jogo.rodadas))
